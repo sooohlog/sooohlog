fc1b0ee6347a39a1cf939e5e23f2d5701671f1e7
sooohlog <sooohblog@gmail.com>
2017-03-19 20:22:15 +0900

    20170319

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
+app.set("view engine", 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+var data = {count : 0};
 app.get('/', function(req, res){
-  res.send('Hello World!');
+  data.count++;
+  res.render('my_first_ejs', data);
 });
+
+app.get('/reset', function(req, res){
+  data.count = 0;
+  res.render('my_first_ejs', data);
+});
+
+app.get('/set/count', function(req, res){
+  if(req.query.count) data.count = req.query.count;
+  res.render('my_first_ejs', data);
+});
+
+app.get('/set/:num', function(req, res){
+  data.count = req.params.num;
+  res.render('my_first_ejs', data);
+});
+
+//console.log(__dirname);
 
 app.listen(3000, function() {
   console.log('Server On!');
 });
+
+
+//app.get('/', function(req, res){
+//  res.send('Hello World!');
+//});
+
+//app.use(express.static(__dirname + '/'));
